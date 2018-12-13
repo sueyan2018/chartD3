bbf3e3ccdda229e248c020e2103008d0c6022070
sueyan <sueyan2018@gmail.com>
2018-12-13 15:00:06 +1100

    modify ui test

--- app/component/index/Index.jsx
+++ app/component/index/Index.jsx
@@ -1,61 +1,87 @@
-import React from 'react';
-import h from 'react-hyperscript'
-import Grid from '@material-ui/core/Grid'
-import Card, {
-    CardContent
-} from '@material-ui/core/Card';
+// import React from 'react';
 
-const styles = {
-    container: {
-        padding: 16,
-    },
-    item: {
-        background: '#EEE'
-    }
-}
+// class Index extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 这是首页
+//             </div>
+//         );
+//     }
+// }
+
+// export default Index;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
 class Index extends React.Component {
-
     constructor(props) {
-        super(props)
-        this.state = {}
+        super(props);
+        this.state = {
+            spacing: '16',
+        };
     }
 
-    render() {
+//   handleChange = key => (event, value) => {
+//     this.setState({
+//       [key]: value,
+//     });
+//   };
 
-        let items = []
-        for (let i = 0; i < 12; i++) {
-            items.push(h(Grid, {
-                item: true,
-                xs: 12,
-                sm: 6,
-                md: 4,
-                lg: 3,
-                style: styles.item,
-            }, [
-                    h(Card, [
-                        h(CardContent, 'Hello Card!')
-                    ]),
-                ]))
-        }
+  render() {
+    const { classes } = this.props;
+    const { spacing } = this.state;
 
-        return h(Grid, {
-            container: true,
-            spacing: 16,
-            style: styles.container,
-        }, items)
-    }
-
-
-    // render() {
-    //     return (
-    //         <div>
-    //             这是首页
-    //         </div>
-    //     );
-    // }
-
-
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={12}>
+          <Grid container className={classes.demo} justify="left" spacing={Number(spacing)}>
+              <Grid item>
+                <Paper className={classes.paper} />
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper} />
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper} />
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper} />
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper} />
+              </Grid>
+            {/* {[0, 1, 2].map(value => (
+              <Grid key={value} item>
+                <Paper className={classes.paper} />
+              </Grid>
+            ))} */}
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default Index;
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Index);
